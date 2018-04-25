61588e49764380daaa60195ca51ac3448645308d
oleglacto <oleglacto@gmail.com>
2018-04-25 21:51:39 +0300

    Some fixes

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,6 +11,12 @@ namespace App\Http\Controllers;
 
 class TestController extends Controller
 {
+
+    public function __construct()
+    {
+
+    }
+
     public function index()
     {
         return view('test.index');
