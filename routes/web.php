5de6ad8a5e7bbc7483284b757427ed8109ed843c
oleglacto <oleglacto@gmail.com>
2018-06-02 11:59:24 +0300

    create routs for cake, and admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,28 @@
 
 Auth::routes();
 
-Route::get('/', 'TestController@index')->name('home');
-Route::get('/home', 'TestController@index')->name('home');
+Route::get('/', 'SiteController@index')->name('home');
+
+// регистрация
+Route::get('/registration', 'Auth\RegisterController@create');
+Route::post('/registration', 'Auth\RegisterController@store');
+
+// Вход/выход
+Route::get('/login', 'Auth\LoginController@create');
+Route::post('/login', 'Auth\LoginController@store');
+Route::get('/logout', 'Auth\LoginController@destroy');
 
 
-
-Route::get('/registration', 'Auth\RegistrationController@create');
-Route::post('/registration', 'Auth\RegistrationController@store');
-
-
-Route::get('/login', 'Auth\SessionsController@create');
-Route::post('/login', 'Auth\SessionsController@store');
-
-Route::get('/logout', 'Auth\SessionsController@destroy');
-
+// что-то тестовое
 Route::get('/second', 'SecondController@index');
 
+// Админка
+Route::get('/admin', 'AdministrationController@index');
+Route::get('/admin/products', 'AdministrationController@products');
 
+// Тортики
+Route::get('/admin/cake', 'CakeController@create')->name('cakeCreate');
+Route::post('/admin/cake', 'CakeController@store')->name('cakeSave');
+Route::get('/admin/cake/{cake}', 'CakeController@edit')->name('cakeEdit');
+Route::put('/admin/cake/{cake}', 'CakeController@update')->name('cakeUpdate');
+Route::delete('/admin/cake/{cake}', 'CakeController@destroy')->name('cakeDelete');
